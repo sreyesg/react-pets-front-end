1b0d9237ccf56cc4fe3bda1518c17a158e23655f
sreyesg <santiagoreyes.g@gmail.com>
2024-11-08 10:28:42 -0800

    Init commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello Archie</h1>
+    </>
+  )
+}
+
+export default App
